9278aa328b65da689ea257ef4521a4a303d4439f
Fernando Blat <ferblape@gmail.com>
2018-05-30 19:06:39 +0200

    Accept sub budget lines

--- lib/gobierto_data/gobierto_budgets/budget_lines_importer.rb
+++ lib/gobierto_data/gobierto_budgets/budget_lines_importer.rb
@@ -16,6 +16,13 @@ module GobiertoData
         data.each do |budget_line|
           id = [budget_line["organization_id"], year, budget_line["code"], budget_line["kind"]].join("/")
           area_name = budget_line.delete("type")
+          if area_name == "economic-custom"
+            area_name = GobiertoData::GobiertoBudgets::ECONOMIC_AREA_NAME
+            id = [budget_line["organization_id"], year, [budget_line["custom_code"], budget_line["code"], "c"].join("/"), budget_line["kind"]].join("/")
+          elsif area_name == "economic-functional"
+            area_name = GobiertoData::GobiertoBudgets::ECONOMIC_AREA_NAME
+            id = [budget_line["organization_id"], year, [budget_line["functional_code"], budget_line["code"], "f"].join("/"), budget_line["kind"]].join("/")
+          end
 
           budget_lines.push({
             index: {
