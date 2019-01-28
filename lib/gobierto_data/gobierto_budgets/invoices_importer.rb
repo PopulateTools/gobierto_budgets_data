32eabfeebf19db62d918859d6d6a945dbe49eae2
Fernando Blat <ferblape@gmail.com>
2019-01-28 11:28:57 +0100

    Invoices importer

---
+++ lib/gobierto_data/gobierto_budgets/invoices_importer.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module GobiertoData
+  module GobiertoBudgets
+    class InvoicesImporter
+      def initialize(options)
+        @index = GobiertoData::GobiertoBudgets::ES_INDEX_INVOICES
+        @type = GobiertoData::GobiertoBudgets::INVOICE_TYPE
+        @data = options.fetch(:data)
+      end
+
+      attr_reader :index, :data, :type
+
+      def import!
+        invoices = []
+        data.each do |attributes|
+          id = [attributes[:location_id], date.year, attributes[:invoice_id]].join('/')
+
+          invoices.push({
+            index: {
+              _index: index,
+              _id: id,
+              _type: type,
+              data: attributes
+            }
+          })
+        end
+
+        GobiertoData::GobiertoBudgets::SearchEngine.client.bulk(body: invoices)
+
+        return invoices.length
+      end
+    end
+  end
+end
