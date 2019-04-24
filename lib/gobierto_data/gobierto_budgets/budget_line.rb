30b3752b8c64fea5f48a807e38a08f63ea41ab16
Alberto Miedes <albertomg994@gmail.com>
2019-04-24 14:06:40 +0200

    Support searching updated data with BudgetLine class

--- lib/gobierto_data/gobierto_budgets/budget_line.rb
+++ lib/gobierto_data/gobierto_budgets/budget_line.rb
@@ -6,6 +6,7 @@ module GobiertoData
 
       def self.all(options)
         area_name = options[:area_name]
+        updated_forecast = options.delete(:updated_forecast) || false
 
         terms = [
           { missing: { field: "functional_code"} },
@@ -33,7 +34,17 @@ module GobiertoData
           size: 10_000
         }
 
-        response = SearchEngine.client.search(index: GobiertoData::GobiertoBudgets::ES_INDEX_FORECAST, type: area_name, body: query)
+        index = if updated_forecast
+                  GobiertoData::GobiertoBudgets::ES_INDEX_FORECAST_UPDATED
+                else
+                  default_index
+                end
+
+        response = SearchEngine.client.search(index: index, type: area_name, body: query)
+
+        if updated_forecast && response["hits"]["hits"].empty?
+          response = SearchEngine.client.search(index: default_index, type: area_name, body: query)
+        end
 
         response["hits"]["hits"].map{ |h| h["_source"] }.map do |row|
           BudgetLinePresenter.new(row.merge(
@@ -52,6 +63,10 @@ module GobiertoData
 
       def self.permitted_terms
         %i[kind organization_id level]
+      end
+
+      def self.default_index
+        GobiertoData::GobiertoBudgets::ES_INDEX_FORECAST
       end
 
     end
