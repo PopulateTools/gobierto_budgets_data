b25ee9649d954b9b9fa8b9a34a9948a2f7a17c45
Alberto Miedes <albertomg994@gmail.com>
2020-01-16 16:56:47 +0100

    Anonymize NIEs and DNIs

--- lib/gobierto_data/gobierto_budgets/invoices_importer.rb
+++ lib/gobierto_data/gobierto_budgets/invoices_importer.rb
@@ -13,6 +13,9 @@ module GobiertoData
 
       attr_reader :index, :data, :type
 
+      DNI_REGEX = /^(\d{8})([A-Z])$/i.freeze
+      NIE_REGEX = /^[XYZ]\d{7,8}[A-Z]$/i.freeze
+
       def import!
         invoices = []
         nitems = 0
@@ -21,23 +24,29 @@ module GobiertoData
           date = Date.parse(attributes["date"])
           id = [attributes["location_id"], date.year, Digest::MD5.hexdigest(attributes["invoice_id"])].join('/')
 
-          invoices.push({
+          original_provider_id = attributes["provider_id"]
+
+          if original_provider_id.match?(DNI_REGEX) || original_provider_id.match?(NIE_REGEX)
+            attributes["provider_id"] = Digest::SHA256.hexdigest(original_provider_id)
+          end
+
+          invoices.push(
             index: {
               _index: index,
               _id: id,
               _type: type,
               data: attributes,
             }
-          })
+          )
 
-          if nitems%300 == 0
+          if (nitems % 300).zero?
             GobiertoData::GobiertoBudgets::SearchEngineWriting.client.bulk(body: invoices)
             invoices = []
           end
         end
         GobiertoData::GobiertoBudgets::SearchEngineWriting.client.bulk(body: invoices)
 
-        return nitems
+        nitems
       end
     end
   end
