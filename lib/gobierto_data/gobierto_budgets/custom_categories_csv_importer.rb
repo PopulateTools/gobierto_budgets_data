b896310fc52e452d2661b96de9cb1f2e43dca189
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-09-21 12:40:48 +0200

    Add class to create/update custom budget lines categories imported from a CSV

---
+++ lib/gobierto_data/gobierto_budgets/custom_categories_csv_importer.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module GobiertoData
+  module GobiertoBudgets
+    class CustomCategoriesCsvImporter
+      attr_reader :csv, :site, :locale
+
+      def initialize(csv, **opts)
+        @csv = csv
+        @site = opts.fetch(:site)
+        @locale = opts.fetch(:locale, site.configuration.default_locale).to_s
+      end
+
+      def import!
+        updated = created = 0
+
+        csv.each do |row|
+          name_translations = { locale => row.field("name") }
+          kinds.each do |kind|
+            category_attrs = {
+              site: site,
+              area_name: CUSTOM_AREA_NAME,
+              kind: kind,
+              code: row.field("code")
+            }
+            if (category = ::GobiertoBudgets::Category.where(category_attrs).first)
+              category.update!(custom_name_translations: (category.custom_name_translations.presence || {}).merge(name_translations))
+              updated += 1
+            else
+              ::GobiertoBudgets::Category.create!(
+                category_attrs.merge(custom_name_translations: name_translations)
+              )
+              created += 1
+            end
+
+          end
+        end
+
+        created + updated
+      end
+
+      private
+
+      def kinds
+        [EXPENSE, INCOME]
+      end
+    end
+  end
+end
