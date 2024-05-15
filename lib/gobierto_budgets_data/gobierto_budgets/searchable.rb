9f370ad39851ca2466149217745d5a8e2c4f7959
Fernando Blat <fernando@blat.es>
2024-05-15 11:47:24 +0200

    Use gem categories and descriptions

---
+++ lib/gobierto_budgets_data/gobierto_budgets/searchable.rb
@@ -0,0 +1,32 @@
+module GobiertoBudgetsData
+  module GobiertoBudgets
+    module Searchable
+      extend ActiveSupport::Concern
+
+      class_methods do
+
+        def all_items
+          @all_items ||= begin
+            all_items = {}
+
+            I18n.available_locales.each do |locale|
+              all_items[locale] ||= {}
+              GobiertoBudgetsData::GobiertoBudgets::ALL_AREAS_NAMES.each do |area_name|
+                spec = Gem::Specification.find_by_name "gobierto_budgets_data"
+
+                file_path = "#{spec.gem_dir}/data/gobierto_budgets/#{area_name}_#{locale}.json"
+                next unless File.exist?(file_path)
+                all_items[locale][area_name] ||= Oj.load(File.read(file_path))
+              end
+            end
+
+            all_items
+          end
+
+          @all_items[I18n.locale][area_name]
+        end
+
+      end
+    end
+  end
+end
