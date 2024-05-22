6dae28f54ffb221ff9b25128750c568a6aed965c
Fernando Blat <fernando@blat.es>
2024-05-22 21:12:08 +0200

    Fix importers

--- lib/gobierto_budgets_data/gobierto_budgets/budget_lines_importer.rb
+++ lib/gobierto_budgets_data/gobierto_budgets/budget_lines_importer.rb
@@ -14,22 +14,21 @@ module GobiertoBudgetsData
       def import!
         budget_lines = []
         data.each do |budget_line|
-          id = [budget_line["organization_id"], year, budget_line["code"], budget_line["kind"]].join("/")
           area_name = budget_line.delete("type")
+          id = [budget_line["organization_id"], year, budget_line["code"], budget_line["kind"], area_name].join("/")
           if area_name == "economic-custom"
             area_name = GobiertoBudgetsData::GobiertoBudgets::ECONOMIC_AREA_NAME
-            id = [budget_line["organization_id"], year, [budget_line["custom_code"], budget_line["code"], "c"].join("/"), budget_line["kind"]].join("/")
+            id = [budget_line["organization_id"], year, [budget_line["custom_code"], budget_line["code"], "c"].join("/"), budget_line["kind"], area_name].join("/")
           elsif area_name == "economic-functional"
             area_name = GobiertoBudgetsData::GobiertoBudgets::ECONOMIC_AREA_NAME
-            id = [budget_line["organization_id"], year, [budget_line["functional_code"], budget_line["code"], "f"].join("/"), budget_line["kind"]].join("/")
+            id = [budget_line["organization_id"], year, [budget_line["functional_code"], budget_line["code"], "f"].join("/"), budget_line["kind"], area_name].join("/")
           end
 
           budget_lines.push({
             index: {
               _index: index,
               _id: id,
-              _type: area_name,
-              data: budget_line
+              data: budget_line.merge(type: area_name)
             }
           })
         end
