5f15d54a7265b159b0f008bafd56d95457ab7733
Fernando Blat <fernando@blat.es>
2024-06-14 16:10:50 +0200

    Add new population types

--- lib/gobierto_budgets_data/gobierto_budgets/search_engine_configuration/data.rb
+++ lib/gobierto_budgets_data/gobierto_budgets/search_engine_configuration/data.rb
@@ -13,6 +13,14 @@ module GobiertoBudgetsData
           GobiertoBudgetsData::GobiertoBudgets::POPULATION_TYPE
         end
 
+        def self.type_population_province
+          GobiertoBudgetsData::GobiertoBudgets::POPULATION_PROVINCE_TYPE
+        end
+
+        def self.type_population_autonomy
+          GobiertoBudgetsData::GobiertoBudgets::POPULATION_AUTONOMY_TYPE
+        end
+
         def self.type_debt
           GobiertoBudgetsData::GobiertoBudgets::DEBT_TYPE
         end
